0104599b1619d9aca0d953c4416db33fff59ddf5
high <highcloud100@inha.edu>
2023-10-26 00:49:40 +0900

    logging , dataflow

--- src/mm4.cpp
+++ src/mm4.cpp
@@ -17,8 +17,10 @@ extern "C"
 						for(int k=0;k<M;k++){
 							
 							for(int ii=0;ii <M/DSIZE;ii++){ // M size의 배열이 필요하기에 m/DSIZE만큼 반복
+							 hls::print("changeA_rate : AStreamWide reading\n");
 								hls::vector<DTYPE, DSIZE> A_temp = AStreamWide.read();
 								for(int i=0;i<DSIZE;i++){
+										hls::print("changeA_rate : AStream writing\n");
 									AStream.write(A_temp[i]);
 								}
 							}
@@ -36,6 +38,7 @@ extern "C"
 				for(int ib=0;ib< N/M; ib++){ // 블럭 아래로 하나씩 
 					for(int i=0;i<M;i++){ // 블럭 내에서 아래로 이동
 						for(int k=0;k<M/DSIZE;k++){ // 블럭 내에서 한줄 읽기
+							hls::print("ReadAt : AStreamWide writing\n");
 							AStreamWide.write(At[((ib*M+i)*N + kb*M)/DSIZE + k]);
 						}
 					}
@@ -52,6 +55,7 @@ extern "C"
 				for(int kb=0;kb<N/M;kb++){ // 아래로 이동 (블록)
 					for(int k=0;k<M;k++){ // 블록 내에서 아래로 이동 (행)
 						for(int jj=0;jj<M/DSIZE;jj++){ // 블록 내에서 한줄 읽기
+						 	hls::print("ReadB : BStream writing\n");
 							BStream.write(B[((kb*M+k)*N+jb*M)/DSIZE+jj]);
 						}
 					}
@@ -68,10 +72,13 @@ extern "C"
 				for(int kb=0;kb< N/M;kb++){
 					for(int k=0;k<M;k++){
 						for(int i=0;i<M;i++){
+							hls::print("Comp : AStream reading\n");
 							DTYPE Ai = AStream.read();
 							for(int jj=0;jj<M/DSIZE;jj++){
+									hls::print("Comp : BStream reading\n");
 								hls::vector<DTYPE, DSIZE> B_temp = BStream.read();
 								for(int j=0;j<DSIZE;j++){
+										hls::print("Comp : ABStream writing\n");
 									ABStream.write(Ai*B_temp[j]);
 								}
 							}
@@ -91,6 +98,7 @@ extern "C"
 						for(int i=0;i<M;i++){
 							
 							for(int j=0;j<DSIZE;j++){
+									hls::print("writeAB : ABStream reading\n");
 								AB[((ib*M+i)*N+jb*M)/DSIZE + j] = ABStream.read();
 							}
 							
@@ -107,6 +115,8 @@ extern "C"
 #pragma HLS INTERFACE mode = m_axi bundle = m0 port = At
 #pragma HLS INTERFACE mode = m_axi bundle = m1 port = B
 #pragma HLS INTERFACE mode = m_axi bundle = m1 port = AB
+
+#pragma HLS DATAFLOW
 
 	hls::stream<hls::vector<DTYPE, DSIZE> > AStreamWide("AStreamWide");
 	hls::stream<DTYPE> AStream("AStream");
