f28fb565390b4fe7e8d6b276d5aaf7434e8da865
high <highcloud100@inha.edu>
2023-10-26 10:36:56 +0900

    test

--- src/host_transposed.cpp
+++ src/host_transposed.cpp
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
 	} 	
 	printf("Done initializing vectors\n");
 
-	for(int i=0;i<1;i++){
+	for(int i=0;i<2;i++){
 		for(int j=0;j<256;j++){
 			printf("%d ", At[i*SIZE+j]);
 		}
